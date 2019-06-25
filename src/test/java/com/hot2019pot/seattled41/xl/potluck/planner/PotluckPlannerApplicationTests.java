63f4a5cbbb96a863134b4c2a24d095812ab74e66
Roger Huba <charm --wait>
2019-06-25 14:59:46 -0700

    update mocuser not working

--- src/test/java/com/hot2019pot/seattled41/xl/potluck/planner/PotluckPlannerApplicationTests.java
+++ src/test/java/com/hot2019pot/seattled41/xl/potluck/planner/PotluckPlannerApplicationTests.java
@@ -1,15 +1,22 @@
 package com.hot2019pot.seattled41.xl.potluck.planner;
 
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
 import java.sql.Date;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -110,11 +117,14 @@ public class PotluckPlannerApplicationTests {
 	}
 
 	@Test
+	@WithMockUser
 	public void testPotluckSignedInRoutePass() throws Exception {
-		// Need to add testing for this.
-		// this.mockMvc.perform(get("/Potluck")).andExpect(status().is3xxRedirection());
-		PotluckUser testUser = createTestUser();
-		Potluck testPotluck = createTestPotluck(testUser);
+		mockMvc.perform(MockMvcRequestBuilders.get("/Potluck")
+				.accept(MediaType.ALL))
+				.andExpect(status().isOk());
+
+//		this.mockMvc.perform(get("/Potluck")).andExpect(status().isOk());
+
 	}
 
 	@Test
@@ -257,5 +267,12 @@ public class PotluckPlannerApplicationTests {
 		// new user add
 		// show user on creator page
 		// add item
+	}
+
+	@Test
+	@WithMockUser
+	public void getMessageWithMockUser() {
+		
+...
 	}
 }
